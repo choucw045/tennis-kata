7bbd6f5fb43691b60511e302aef74f663595081f
tomsoraka <a932l41l4@gmail.com>
2023-11-18 23:12:59 +0800

    reuslt 14:58

--- TennisKata.Tests/TennisGameTests.cs
+++ TennisKata.Tests/TennisGameTests.cs
@@ -2,14 +2,124 @@ namespace TennisKata.Tests;
 
 public class Tests
 {
+    private TennisGame _tennisGame = null!;
+
     [SetUp]
     public void Setup()
     {
+        _tennisGame = new TennisGame(Player1, Player2);
+    }
+
+    private const string Player1 = "Bob";
+
+    [Test]
+    public void LoveAll()
+    {
+        ScoreShouldBe("Love All");
     }
 
     [Test]
-    public void Test1()
+    public void FifteenLove()
     {
-        Assert.Pass();
+        GivenScore(1, Player1);
+        ScoreShouldBe("Fifteen Love");
+    }
+
+    [Test]
+    public void ThirtyLove()
+    {
+        GivenScore(2, Player1);
+        ScoreShouldBe("Thirty Love");
+    }
+
+    [Test]
+    public void FortyLove()
+    {
+        GivenScore(3, Player1);
+        ScoreShouldBe("Forty Love");
+    }
+
+    [Test]
+    public void LoveFifteen()
+    {
+        GivenScore(1, Player2);
+        ScoreShouldBe("Love Fifteen");
+    }
+
+    [Test]
+    public void LoveThirty()
+    {
+        GivenScore(2, Player2);
+        ScoreShouldBe("Love Thirty");
+    }
+
+    [Test]
+    public void FifteenAll()
+    {
+        GivenScore(1, Player1);
+        GivenScore(1, Player2);
+        ScoreShouldBe("Fifteen All");
+    }
+
+    [Test]
+    public void ThirtyAll()
+    {
+        GivenScore(2, Player1);
+        GivenScore(2, Player2);
+        ScoreShouldBe("Thirty All");
+    }
+
+    [Test]
+    public void Deuce()
+    {
+        GivenDeuce();
+        ScoreShouldBe("Deuce");
+    }
+
+    [Test]
+    public void FirstPlayerAdv()
+    {
+        GivenDeuce();
+        GivenScore(1, Player1);
+        ScoreShouldBe($"{Player1} Adv");
+    }
+
+    [Test]
+    public void SecondPlayerAdv()
+    {
+        GivenDeuce();
+        GivenScore(1, Player2);
+        ScoreShouldBe($"{Player2} Adv");
+    }
+
+    [Test]
+    public void SecondPlayerWin()
+    {
+        GivenDeuce();
+        GivenScore(2, Player2);
+        ScoreShouldBe($"{Player2} Win");
+    }
+
+    private void GivenDeuce()
+    {
+        GivenScore(3, Player1);
+        GivenScore(3, Player2);
+    }
+
+
+    private const string Player2 = "Alice";
+
+
+    private void GivenScore(int score, string playerName)
+    {
+        for (int j = 0; j < score; j++)
+        {
+            _tennisGame.PlayerScore(playerName);
+        }
+    }
+
+    private void ScoreShouldBe(string expected)
+    {
+        _tennisGame.GetScore().Should().Be(expected);
     }
 }
--- TennisKata/TennisGame.cs
+++ TennisKata/TennisGame.cs
@@ -2,8 +2,75 @@
 
 public class TennisGame
 {
+    private int _firstPlayerCurrentScore;
+
+    private static readonly Dictionary<int, string> ScoreMap = new()
+    {
+        { 0, "Love" },
+        { 1, "Fifteen" },
+        { 2, "Thirty" },
+        { 3, "Forty" },
+    };
+
+    private readonly string _firstPlayerName;
+    private readonly string _secondPlayerName;
+    private int _secondPlayerCurrentScore;
+    private bool _deuce;
+
+    public TennisGame(string firstPlayerName, string secondPlayerName)
+    {
+        _firstPlayerName = firstPlayerName;
+        _secondPlayerName = secondPlayerName;
+    }
+
     public string GetScore()
     {
-        throw new NotImplementedException();
+        if (_deuce)
+        {
+            if (IsSameScore())
+                return "Deuce";
+
+            if (IsAdv() == 1)
+                return $"{GetLeadPlayer()} Adv";
+
+            return $"{GetLeadPlayer()} Win";
+        }
+
+        if (_firstPlayerCurrentScore < 1 && _secondPlayerCurrentScore < 1) return "Love All";
+
+        var score1 = GetScoreDesc(_firstPlayerCurrentScore);
+        var score2 = GetScoreDesc(_secondPlayerCurrentScore);
+        return _firstPlayerCurrentScore == _secondPlayerCurrentScore ? $"{score1} All" : $"{score1} {score2}";
+    }
+
+    private int IsAdv()
+    {
+        return Math.Abs(_firstPlayerCurrentScore - _secondPlayerCurrentScore);
+    }
+
+    private bool IsSameScore()
+    {
+        return _firstPlayerCurrentScore == _secondPlayerCurrentScore;
+    }
+
+    private string GetLeadPlayer()
+    {
+        return _firstPlayerCurrentScore > _secondPlayerCurrentScore ? _firstPlayerName : _secondPlayerName;
+    }
+
+    private static string GetScoreDesc(int firstPlayerCurrentScore)
+    {
+        return ScoreMap[firstPlayerCurrentScore];
+    }
+
+    public void PlayerScore(string playerName)
+    {
+        if (playerName == _firstPlayerName)
+            _firstPlayerCurrentScore++;
+        if (playerName == _secondPlayerName)
+            _secondPlayerCurrentScore++;
+
+        if (!_deuce && _firstPlayerCurrentScore == _secondPlayerCurrentScore && _firstPlayerCurrentScore == 3)
+            _deuce = true;
     }
 }
