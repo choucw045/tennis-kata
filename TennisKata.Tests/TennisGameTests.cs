11f1ad2e68ca85d2cfc86889a5292bc0dbb371a0
tomsoraka <a932l41l4@gmail.com>
2023-11-19 02:02:58 +0800

    try very simple TDD but I really don't like the code though.

--- TennisKata.Tests/TennisGameTests.cs
+++ TennisKata.Tests/TennisGameTests.cs
@@ -2,14 +2,134 @@ namespace TennisKata.Tests;
 
 public class Tests
 {
+    private TennisGame _tennisGame = null!;
+
     [SetUp]
     public void Setup()
     {
+        _tennisGame = new TennisGame(FirstPlayer, SecondPlayer);
     }
 
     [Test]
-    public void Test1()
+    public void LoveAll()
     {
-        Assert.Pass();
+        ScoreShouldBe("Love All");
+    }
+
+    [Test]
+    public void FifteenLove()
+    {
+        _tennisGame.FirstPlayerScore();
+        ScoreShouldBe("Fifteen Love");
+    }
+
+    [Test]
+    public void ThirtyLove()
+    {
+        GivenFirstPlayerScore(2);
+        ScoreShouldBe("Thirty Love");
+    }
+
+    [Test]
+    public void FortyLove()
+    {
+        GivenFirstPlayerScore(3);
+        ScoreShouldBe("Forty Love");
+    }
+
+    [Test]
+    public void LoveFifteen()
+    {
+        _tennisGame.SecondPlayerScore();
+        ScoreShouldBe("Love Fifteen");
+    }
+
+    [Test]
+    public void LoveThirty()
+    {
+        GivenSecondPlayerScore(2);
+        ScoreShouldBe("Love Thirty");
+    }
+
+    [Test]
+    public void FifteenAll()
+    {
+        GivenSecondPlayerScore(1);
+        GivenFirstPlayerScore(1);
+        ScoreShouldBe("Fifteen All");
+    }
+
+    [Test]
+    public void ThirtyAll()
+    {
+        GivenSecondPlayerScore(2);
+        GivenFirstPlayerScore(2);
+        ScoreShouldBe("Thirty All");
+    }
+
+    [Test]
+    public void Deuce()
+    {
+        GivenDeuce();
+        ScoreShouldBe("Deuce");
+    }
+
+    [Test]
+    public void FirstPlayerAdv()
+    {
+        GivenDeuce();
+        GivenFirstPlayerScore(1);
+        ScoreShouldBe($"{FirstPlayer} Adv");
+    }
+
+    [Test]
+    public void SecondPlayerAdv()
+    {
+        GivenDeuce();
+        GivenSecondPlayerScore(1);
+        ScoreShouldBe($"{SecondPlayer} Adv");
+    }
+
+    [Test]
+    public void SecondPlayerWin()
+    {
+        GivenDeuce();
+        GivenSecondPlayerScore(2);
+        ScoreShouldBe($"{SecondPlayer} Win");
+    }
+
+
+    private const string SecondPlayer = "Bob";
+
+    private void GivenDeuce()
+    {
+        GivenSecondPlayerScore(3);
+        GivenFirstPlayerScore(3);
+    }
+
+    private const string FirstPlayer = "Alice";
+
+
+    private void GivenSecondPlayerScore(int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            _tennisGame.SecondPlayerScore();
+        }
+    }
+
+
+    private void GivenFirstPlayerScore(int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            _tennisGame.FirstPlayerScore();
+        }
+    }
+
+
+    private void ScoreShouldBe(string expected)
+    {
+        _tennisGame.GetScore().Should().Be(expected);
     }
 }
--- TennisKata/TennisGame.cs
+++ TennisKata/TennisGame.cs
@@ -1,9 +1,65 @@
 ﻿namespace TennisKata;
 
-public class TennisGame
+public class TennisGame(string firstPlayer, string secondPlayer)
 {
+    private readonly string _firstPlayer = firstPlayer;
+    private readonly string _secondPlayer = secondPlayer;
+    private int _firstPlayerScoreTimes;
+
+    private readonly Dictionary<int, string> _scoreMap = new()
+    {
+        { 0, "Love" },
+        { 1, "Fifteen" },
+        { 2, "Thirty" },
+        { 3, "Forty" },
+    };
+
+    private int _secondPlayerScoreTimes;
+
     public string GetScore()
     {
-        throw new NotImplementedException();
+        if (IsDeuce())
+        {
+            if (IsSameScore())
+                return "Deuce";
+            return IsAdv() ? $"{GetAdvPlayer()} Adv" : $"{GetAdvPlayer()} Win";
+        }
+
+        if (IsSameScore())
+        {
+            return $"{_scoreMap[_firstPlayerScoreTimes]} All";
+        }
+
+        return $"{_scoreMap[_firstPlayerScoreTimes]} {_scoreMap[_secondPlayerScoreTimes]}";
+    }
+
+    private bool IsSameScore()
+    {
+        return _firstPlayerScoreTimes == _secondPlayerScoreTimes;
+    }
+
+    private bool IsAdv()
+    {
+        return Math.Abs(_firstPlayerScoreTimes - _secondPlayerScoreTimes) == 1;
+    }
+
+    private bool IsDeuce()
+    {
+        return _firstPlayerScoreTimes >= 3 && _secondPlayerScoreTimes >= 3;
+    }
+
+    private string GetAdvPlayer()
+    {
+        return _firstPlayerScoreTimes > _secondPlayerScoreTimes ? _firstPlayer : _secondPlayer;
+    }
+
+    public void FirstPlayerScore()
+    {
+        _firstPlayerScoreTimes++;
+    }
+
+    public void SecondPlayerScore()
+    {
+        _secondPlayerScoreTimes++;
     }
 }
